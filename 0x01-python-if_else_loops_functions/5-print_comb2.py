be02b6c92a613b1e6ed3c0b43277e90cf041a87b
MildredJessica <nwonyejessica@gmail.com>
2023-03-08 22:25:41 +0100

    Print numbers from 0 t0 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for num in range(10):
-    for s in range(10):
-        if s == 9 and num == 9:
-            print("{}{}".format(num, s))
-        else:
-            print("{}{}".format(num, s), end=', ')
+for num in range(100):
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02}".format(num), end=', ')
