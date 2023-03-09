f6636ff72b4e11a7dd68bbda90c179ab2e75c193
MildredJessica <nwonyejessica@gmail.com>
2023-03-09 07:51:36 +0100

    Changed a file

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number in a sorted singly linkedlist
+ * @head: First item in the linkedlist
+ * @number: Number to be added to the linkedlist
+ * Return: The address ofthe new_node or NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{    
+    listint_t *curr;
+    listint_t *temp;
+
+    printf("wrong");
+    temp = malloc(sizeof(listint_t));
+    curr = *head;
+    printf("wrong %i",curr->n);
+    if (temp == NULL)
+    return (NULL);
+    temp->n = number;
+    temp->next = NULL;
+    printf("wronge %i",temp->n);
+
+    if (curr == NULL || curr->n >= number)
+    {
+        temp->next = curr;
+        curr = temp;
+    }
+    
+    else
+    {
+        while ((curr->next != NULL) && (curr->next->n < number))
+        {
+            curr = curr->next;
+        }
+        temp->next = curr->next;
+        curr->next = temp;
+        curr = temp;
+    }
+    return (temp);  
+}
---
+++ 0x01-python-if_else_loops_functions/13-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 5);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+
+    printf("-----------------\n");
+    printf("fix me\n");
+    insert_node(&head, 4);
+
+    print_listint(head);
+
+    free_listint(head);
+
+    return (0);
+}
