2975abe3d2823e84b0f9178fd362130cb21b7fa7
MildredJessica <nwonyejessica@gmail.com>
2023-03-09 07:55:32 +0100

    Changed a file

--- 0x01-python-if_else_loops_functions/13-main.c
+++ 0x01-python-if_else_loops_functions/13-main.c
@@ -17,15 +17,14 @@ int main(void)
     add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 2);
     add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 5);
+    add_nodeint_end(&head, 4);
     add_nodeint_end(&head, 98);
     add_nodeint_end(&head, 402);
     add_nodeint_end(&head, 1024);
     print_listint(head);
 
     printf("-----------------\n");
-    printf("fix me\n");
-    insert_node(&head, 4);
+    insert_node(&head, 27);
 
     print_listint(head);
 
