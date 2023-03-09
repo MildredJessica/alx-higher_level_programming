c6c47dbec60ebc0b173e8aba3f58d56d52f6a7cf
MildredJessica <nwonyejessica@gmail.com>
2023-03-09 05:24:50 +0100

    Added a break statement

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@ for num in range(0, 10):
     for s in range(num + 1, 10):
         if s == 9 and num == 8:
             print("{}{}".format(num, s))
+            break
         print("{}{}".format(num, s), end=', ')
