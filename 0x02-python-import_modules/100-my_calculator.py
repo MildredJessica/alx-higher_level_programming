c3e8ee925b8d776e1c9ce860d623f5709e6a282f
MildredJessica <nwonyejessica@gmail.com>
2023-03-10 11:02:29 +0100

    Added pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Imports all functions from the file calculator_1.py and handles basic operations"""
+    """Imports all functions from the file
+    calculator_1.py and handles basic operations"""
     from calculator_1 import add, sub, div, mul
     import sys
 
     count = len(sys.argv)
     for i in range(count):
         print("{} {}". format(i, sys.argv[i]))
-    operators = {"+": add, "-": sub, "s*": mul, "/": div}
-    if sys.argv[2] not in list(operators.keys()):
+    op = {"+": add, "-": sub, "s*": mul, "/": div}
+    if sys.argv[2] not in list(op.keys()):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
