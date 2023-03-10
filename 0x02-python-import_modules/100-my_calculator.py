78258e096bfe18dbfd625a6bd548933302b2724f
MildredJessica <nwonyejessica@gmail.com>
2023-03-10 11:00:18 +0100

    Handles all basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Imports all functions from the file calculator_1.py and handles basic operations"""
+    from calculator_1 import add, sub, div, mul
+    import sys
+
+    count = len(sys.argv)
+    for i in range(count):
+        print("{} {}". format(i, sys.argv[i]))
+    operators = {"+": add, "-": sub, "s*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
