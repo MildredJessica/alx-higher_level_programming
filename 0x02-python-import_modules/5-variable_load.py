b4fe24cbd10a1232539aba085abc007f39d82c47
MildredJessica <nwonyejessica@gmail.com>
2023-03-10 10:05:35 +0100

    Imports a variable from a file and prints the result

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Imports the variable a from the file variable_load_5.py and prints its value"""
+    """Imports the variable a from the file variable_load_5.py
+    and prints its value"""
     import variable_load_5 as vr
     print(vr.a)
