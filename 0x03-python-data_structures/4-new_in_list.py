454642135629433e5795c68e03d4b692513d8b71
MildredJessica <nwonyejessica@gmail.com>
2023-03-12 17:50:37 +0100

    Replaces an element in a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    """Replaces an element in a list based on a certain position"""
     if idx < 0 or idx > len(my_list) - 1:
         return None
     new_list = my_list.copy()
