742aaacc9e5e98d702ae9130151a250a572da3c9
MildredJessica <nwonyejessica@gmail.com>
2023-03-14 01:59:14 +0100

    Checks for Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,67 +1,51 @@
 #include "lists.h"
 
+
 /**
- * reverse_listint - Reverses a linkedlist
- * @head: Pointer to the first node in the list
- * Return: Pointer to the first node in the new list
+ * size - Returns the length of a singly-linked .
+ * @head: A pointer to the starting node of the list.
+ *
+ * Return: Tje size.
  */
-void reverse_listint(listint_t **head)
+int size(listint_t **head)
 {
-listint_t *prev = NULL;
-listint_t *current = *head;
-listint_t *next = NULL;
+int counter = 0;
+listint_t *curr = *head;
 
-while (current)
+while (curr != NULL)
 {
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
+curr = curr->next;
+counter++;
 }
-*head = prev;
+return (counter);
 }
 
 /**
- * is_palindrome - Checks if a linked list is a palindrome
- * @head: Double pointer to the linkedlist
- * Return: 0 if there is no palindrome
- *         Otherwise 1
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
  */
 int is_palindrome(listint_t **head)
 {
-listint_t *front, *back, *temp, *dup = NULL;
-
-front = back = temp = *head;
+int i = 0, j;
+listint_t  *front, *rear;
+int count = size(head);
 
 if (*head == NULL || (*head)->next == NULL)
 return (1);
-while (1)
+while (i != count / 2)
 {
-back = back->next->next;
-if (!back)
-{
-dup = front->next;
-break;
-}
-if (!back->next)
-{
-dup = front->next->next;
-break;    
-}
-back = back->next;
-}
-reverse_listint(&dup);
-while (dup && temp)
-{
-if (temp->n == dup->n)
-{
-dup = dup->next;
-temp = temp->next;
-}
+front = rear = *head;
+for (j = 0; j < i; j++)
+front = front->next;
+for (j = 0; j < count - (i + 1); j++)
+rear = rear->next;
+if (front->n != rear->n)
+return (0);
 else
-return (0);
+i++;
 }
-if (!dup)
 return (1);
-return (0);
 }
