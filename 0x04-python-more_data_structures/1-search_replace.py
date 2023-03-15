6405fab86f197f0da2fc1dfc29eaa1a2e75625b7
MildredJessica <nwonyejessica@gmail.com>
2023-03-16 00:19:57 +0100

    Changed the return statement

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 
 def search_replace(my_list, search, replace):
     """Replaces all occurrences of an element by another in a new list"""
-    return ([list(map(lambda t: replace if t == search else t, my_list))])
+    return (list(map(lambda t: replace if t == search else t, my_list)))
