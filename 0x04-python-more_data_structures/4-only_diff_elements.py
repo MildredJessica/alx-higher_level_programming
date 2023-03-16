ea1b1f7fd6d4fecdd3a532c48c08cde2f3cd57b3
MildredJessica <nwonyejessica@gmail.com>
2023-03-16 01:15:26 +0100

    Changed the entire code

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,10 +2,4 @@
 
 def only_diff_elements(set_1, set_2):
     """Returns a set of all elements present in only one set."""
-    set_list = []
-    for i in set_1:
-        set_list.append(i)
-    for i in set_2:
-        if i not in set_list:
-            set_list.append(i)
-    return (set_list)
+    return (set_1 ^ set_2)
