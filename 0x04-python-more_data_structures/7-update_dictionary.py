3cf1a6495e19511951c2e92a654dc46b6f2b42ca
MildredJessica <nwonyejessica@gmail.com>
2023-03-16 01:38:58 +0100

    Replaces or adds a key to a dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    """Replaces or adds a key, value to a dictionary"""
     if key not in a_dictionary.keys():
         a_dictionary[key] = value
     else:
