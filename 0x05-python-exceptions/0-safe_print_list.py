fdbe66d6cbc699ebb88a9f2298fdef083ab42fed
MildredJessica <nwonyejessica@gmail.com>
2023-03-28 00:22:45 +0100

    prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list"""
+    count = 0
+    try:
+        while (count != x):
+            print("{:d}".format(my_list[count]), end="")
+            count+=1
+    except TypeError:
+        print("Unsupported Format")
+    except IndexError:
+        print(" Index out of Range")
+    else:
+        print("")
+    return count
