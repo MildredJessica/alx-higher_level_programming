74022d40af7c1e4061b4fde6bf34b0b02a4da6a9
MildredJessica <nwonyejessica@gmail.com>
2023-03-28 00:48:06 +0100

    Checked using pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         while (count != x):
             print("{:d}".format(my_list[count]), end="")
-            count+=1
+            count += 1
     except TypeError:
         print("Unsupported Format")
     except IndexError:
