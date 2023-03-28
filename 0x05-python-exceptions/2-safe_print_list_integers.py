6b44e83be26ec06fc784f1e0f9c5cf62f1a40159
MildredJessica <nwonyejessica@gmail.com>
2023-03-28 02:20:15 +0100

    Changed the file

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-from curses.ascii import isdigit
-
-
+ 
 def safe_print_list_integers(my_list=[], x=0):
     """Prints the first x elements of a list and only integers"""
     count = 0
