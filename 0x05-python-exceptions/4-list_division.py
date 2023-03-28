2d1f78b05e6834f3ca2bc6749e725e63fae47c88
MildredJessica <nwonyejessica@gmail.com>
2023-03-28 03:33:05 +0100

    ivides element by element 2 lists

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,11 +2,11 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     """Divides element by element 2 lists"""
-    new_list =[]
+    new_list = []
     for i in range(list_length):
         try:
             new_list.append(my_list_1[i] / my_list_2[i])
-        except(IndexError, TypeError, ZeroDivisionError) as err:
+        except (IndexError, TypeError, ZeroDivisionError) as err:
             if (isinstance(err, ZeroDivisionError)):
                 print("division by 0")
             elif (isinstance(err, TypeError)):
