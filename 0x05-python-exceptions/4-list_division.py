b37300a2e6ab37446e627c99f534f711a1c4ff53
MildredJessica <nwonyejessica@gmail.com>
2023-03-28 03:28:43 +0100

    ivides element by element 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides element by element 2 lists"""
+    new_list =[]
+    for i in range(list_length):
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
+        except(IndexError, TypeError, ZeroDivisionError) as err:
+            if (isinstance(err, ZeroDivisionError)):
+                print("division by 0")
+            elif (isinstance(err, TypeError)):
+                print("wrong type")
+            elif (isinstance(err, IndexError)):
+                print("out of range")
+            new_list.append(0)
+        finally:
+            pass
+    return new_list
