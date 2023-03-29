36c3168a802530468555287d831557659f27a716
MildredJessica <nwonyejessica@gmail.com>
2023-03-29 01:34:30 +0100

    Initialization of private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square():
+    """Initialization of private instance attribute
+        Args:
+        size (int): The size of the square
+    """
+    def __init__(self, size):
+        self._size = size
