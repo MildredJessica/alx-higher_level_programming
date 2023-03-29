2fe147a290222251940960f96f67ae32fc663104
MildredJessica <nwonyejessica@gmail.com>
2023-03-29 01:53:44 +0100

    Creates a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square():
+    """Creates a square"""
+    __size = None
+    
+    def __init__(self, size=0):
+        """Initialization of private instance attribute
+            Args:
+                size (int): The size of the square
+        """
+        if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
