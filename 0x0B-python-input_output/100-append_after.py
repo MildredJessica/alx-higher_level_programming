3c7e8c59950a08e39f8f8704333f2fbf026525d5
MildredJessica <nwonyejessica@gmail.com>
2023-04-17 04:52:03 +0100

    reads stdin

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Inserts a line of text to a file, after each line containing a specific string"""
+"""Inserts a line of text to a file, after each
+line containing a specific string"""
 
 
 def append_after(filename="", search_string="", new_string=""):
