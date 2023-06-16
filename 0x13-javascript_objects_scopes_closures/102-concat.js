3b7f3929ccf76ada96c5247284dc65fa7300af15
MildredJessica <nwonyejessica@gmail.com>
2023-06-16 03:46:55 +0100

    chanded the first line

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,4 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 const file = require('fs');
 const fileA = file.readFileSync(process.argv[2], 'utf8');
 const fileB = file.readFileSync(process.argv[3], 'utf8');
