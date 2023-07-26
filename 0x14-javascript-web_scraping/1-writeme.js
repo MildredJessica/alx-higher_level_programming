ca942bb8e4c1b93843c26fe13981da6fe369fc4b
MildredJessica <nwonyejessica@gmail.com>
2023-07-26 05:25:27 +0100

    Writes to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-// Reads and prints the content of a file.
+// Writes to a file.
 
 const arg = process.argv;
 const fs = require('fs');
-const text = arg[2];
+const file = arg[2];
+const text = arg[3];
+fs.writeFile(file, text, 'utf-8', (err) => {
+  if (err) throw err;
+});
